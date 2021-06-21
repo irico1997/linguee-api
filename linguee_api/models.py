2ec481ef3ec8bc9d65265cc79fbf96e05975dc25
Isabel <isabel@MacBook-Pro.local>
2021-06-21 13:45:01 -0700

    trying to fix api :/

--- linguee_api/models.py
+++ linguee_api/models.py
@@ -46,8 +46,8 @@ class SearchResult(BaseModel):
         
         class AltForm(BaseModel):
             """Alternate forms. Only testing on german"""
-            alt_word: list[str] = [Field(example="Frauen")]
-            alt_pos: list[str] = [Field(example="plural")]
+            alt_word: list[str] = Field(example=["Frauen"])
+            alt_pos: list[str] = Field(example=["plural"])
         
         featured: bool = Field(example=False)
         text: str = Field(example="obrigado")
